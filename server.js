28f16217347ce52791faf80df86e69d0cbe97b7e
sakib3 <sakib3@gmail.com>
2017-11-18 07:52:58 +0100

    added router

--- server.js
+++ server.js
@@ -2,10 +2,11 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const config = require('./config');
+const router = require('./router');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.get('/', (req, res) => res.send('Hello ShareRide!'));
+app.use("/", router);
 
 app.listen(config.port, () => console.log('Example app listening on port '+config.port+'!'));
