9102fa1a3e4d8c05ab48bcdd014a004924904a0c
sakib3 <sakib3@gmail.com>
2017-11-18 07:12:27 +0100

    little change to check auto deploy from github to heroku

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
 const config = require('./config');
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.send('Hello ShareRide!'))
 
 app.listen(config.port, () => console.log('Example app listening on port '+config.port+'!'))
