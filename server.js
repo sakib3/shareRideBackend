4b54b6a903db299f5cceb733fa884902c4a6ad02
sakib3 <sakib3@gmail.com>
2017-11-18 07:42:12 +0100

    added body parser

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
 const config = require('./config');
-app.get('/', (req, res) => res.send('Hello ShareRide!'))
 
-app.listen(config.port, () => console.log('Example app listening on port '+config.port+'!'))
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => res.send('Hello ShareRide!'));
+
+app.listen(config.port, () => console.log('Example app listening on port '+config.port+'!'));
