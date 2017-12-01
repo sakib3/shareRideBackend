fad16c21bf24b6c593b2ea45714e871d496fa900
sakib3 <sakib3@gmail.com>
2017-12-01 22:29:00 +0100

    added cros

--- server.js
+++ server.js
@@ -9,6 +9,12 @@ var requireAuthentication = function (req, res, next) {
     next();
 };
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.all('*', requireAuthentication);
